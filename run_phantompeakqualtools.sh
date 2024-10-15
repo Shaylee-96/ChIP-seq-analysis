99490b2d54aacd36e6c40110218771e49f39df38
Shay Lee <106128249+Shaylee-96@users.noreply.github.com>
2024-10-16 00:07:21 +0800

    Update run_phantompeakqualtools.sh

--- run_phantompeakqualtools.sh
+++ run_phantompeakqualtools.sh
@@ -3,6 +3,9 @@
 #Install phantompeakqualtools using anaconda
   #conda install bioconda::phantompeakqualtools
 
+#conda activate the environment
+
+#run the phantompeakqualtools for all the bam files using 12 CPUs
 for i in *.bam
 do
   run_spp.R -c=$i -savp -p=12 -odir=phantompeakqualtools_output -out=$i
